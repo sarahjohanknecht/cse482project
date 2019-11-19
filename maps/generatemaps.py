f947aabf8da61eb92102e698d335047e608ab2ab
sarahjohanknecht <johankn1@msu.edu>
2019-11-19 10:50:15 -0500

    small change sry

--- maps/generatemaps.py
+++ maps/generatemaps.py
@@ -2,21 +2,25 @@ import geopandas as gpd
 import pandas as pd
 import matplotlib.pyplot as plt
 
+NUM_DAYS = 3 # set this to the number of days we are collecting data for PLUS ONE
+
 # read in map shp file
 shp_path = "ne_110m_admin_1_states_provinces/ne_110m_admin_1_states_provinces.shp"
 map_df = gpd.read_file(shp_path)
 
-df_by_day = []
+df_by_day = [] # list to hold each dataframe for each day's data
 
 # read states sentiment data
 df = pd.read_csv('states_with_date.csv')
 print(df.head())
 
-for i in range(1,3):
+#split the data frame into data frames by day
+for i in range(1, NUM_DAYS):
     df_day = df[df['Date'] == i]
     df_day.columns = ["State", "Sent", "Date"]
     df_by_day.append(df_day)
 
+#generate map for each day
 count = 1
 for day in df_by_day:
     print(day)
